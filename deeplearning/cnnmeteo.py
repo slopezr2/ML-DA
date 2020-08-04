1bdaa7b41d454fa56fb5b7b21a99db7f07e403ab
Luis Guzman <lucho@socobox.co>
2020-08-04 09:45:33 -0500

    add regularization and dropout

--- deeplearning/cnnmeteo.py
+++ deeplearning/cnnmeteo.py
@@ -6,48 +6,64 @@ from tensorflow.keras.layers import TimeDistributed
 from tensorflow.keras.layers import Conv1D
 from tensorflow.keras.layers import MaxPooling1D
 from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import ConvLSTM2D
-
-
+from tensorflow.keras import regularizers
 from tensorflow.keras.metrics import RootMeanSquaredError
 
-class CnnMeteo:
 
-    def __init__(self, n_input_steps, n_features, n_output_steps):
+class Regularized_ML:
+    def __init__(self):
         self.model = Sequential()
+
+    def add_regulization(self, reg, drop):
+        if reg:
+            self.model.add(Dense(50, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=0.01, l2=0.02)))
+        else:
+            self.model.add(Dense(50, activation='relu'))
+        if drop:
+            self.model.add(Dropout(0.5))
+
+
+class CnnMeteo(Regularized_ML):
+
+    def __init__(self, n_input_steps, n_features, n_output_steps, reg=False, drop=False):
+        super().__init__()
         self.model.add(LSTM(100, return_sequences=True, input_shape=(n_input_steps, n_features)))
         self.model.add(LSTM(100, return_sequences=True))
         self.model.add(LSTM(100))
-        self.model.add(Dense(50, activation='relu'))
+        self.add_regulization(reg, drop)
         self.model.add(Dense(n_output_steps))
         self.model.compile(optimizer='adam', loss='mse', metrics=[RootMeanSquaredError()])
 
 
-class LstmVanillaMeteo:
+class LstmVanillaMeteo(Regularized_ML):
 
-    def __init__(self, n_input_steps, n_features, n_output_steps):
-        self.model = Sequential()
+    def __init__(self, n_input_steps, n_features, n_output_steps, reg=False, drop=False):
+        super().__init__()
         self.model.add(LSTM(100, input_shape=(n_input_steps, n_features)))
-        self.model.add(Dense(50, activation='relu'))
+        self.add_regulization(reg, drop)
         self.model.add(Dense(n_output_steps))
         self.model.compile(optimizer='adam', loss='mse', metrics=[RootMeanSquaredError()])
 
-class LstmStackedMeteo:
 
-    def __init__(self, n_input_steps, n_features, n_output_steps):
-        self.model = Sequential()
+class LstmStackedMeteo(Regularized_ML):
+
+    def __init__(self, n_input_steps, n_features, n_output_steps, reg=False, drop=False):
+        super().__init__()
         self.model.add(LSTM(100, return_sequences=True, input_shape=(n_input_steps, n_features)))
-        self.model.add(LSTM(100 ))
-        self.model.add(Dense(50, activation='relu'))
+        self.model.add(LSTM(100))
+        self.add_regulization(reg, drop)
         self.model.add(Dense(n_output_steps))
         self.model.compile(optimizer='adam', loss='mse', metrics=[RootMeanSquaredError()])
 
-class LstmBidireccionalMeteo:
 
-    def __init__(self, n_input_steps, n_features, n_output_steps):
-        self.model = Sequential()
-        self.model.add( Bidirectional( LSTM(100,  input_shape=(n_input_steps, n_features))))
-        self.model.add(Dense(50, activation='relu'))
+class LstmBidireccionalMeteo(Regularized_ML):
+
+    def __init__(self, n_input_steps, n_features, n_output_steps, reg=False, drop=False):
+        super().__init__()
+        self.model.add(Bidirectional(LSTM(100, input_shape=(n_input_steps, n_features))))
+        self.add_regulization(reg, drop)
         self.model.add(Dense(n_output_steps))
         self.model.compile(optimizer='adam', loss='mse', metrics=[RootMeanSquaredError()])
 
@@ -57,11 +73,10 @@ class CnvLstmMeteo:
     def __init__(self, n_input_steps, n_features, n_output_steps):
         self.model = Sequential()
         self.model.add(TimeDistributed(Conv1D(64, 2, activation='relu'), input_shape=(None, n_input_steps,
-                                                                                 n_features)))
+                                                                                      n_features)))
         self.model.add(TimeDistributed(MaxPooling1D()))
         self.model.add(TimeDistributed(Flatten()))
         self.model.add(LSTM(50, activation='relu'))
         self.model.add(Dense(50, activation='relu'))
         self.model.add(Dense(n_output_steps))
         self.model.compile(optimizer='adam', loss='mse', metrics=[RootMeanSquaredError()])
-
--- meteo.py
+++ meteo.py
@@ -12,18 +12,15 @@ import warnings
 import json
 import sys
 
-
-
 import tensorflow as tf
-param =  int(sys.argv[1])
+
+param = int(sys.argv[1])
 print(param)
-#Comment to run with GPU o Select CPU
+# Comment to run with GPU o Select CPU
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 for physical_device in physical_devices:
     tf.config.experimental.set_memory_growth(physical_device, True)
-
-
 
 warnings.filterwarnings('ignore')
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -39,36 +36,58 @@ n_input_steps = 24 * 7 * 2
 n_output_steps = 24 * 3
 pre_processor = Combiner()
 X, y = pre_processor.combine(n_input_steps, n_output_steps, station82_t.Value.values, station82_w.Value.values,
-                                     station82_pm25.CONCENTRATION.values, station82_pm25.CONCENTRATION.values)
+                             station82_pm25.CONCENTRATION.values, station82_pm25.CONCENTRATION.values)
 
-
-#Create Model
+# Create Model
 n_train = 9500
 n_features = X.shape[2]
 
-mls = [CnnMeteo(n_input_steps,n_features, n_output_steps),LstmVanillaMeteo(n_input_steps,n_features, n_output_steps),
-       LstmStackedMeteo(n_input_steps,n_features, n_output_steps),LstmBidireccionalMeteo(n_input_steps,n_features, n_output_steps)  ]
-mls_label = ['CnnMeteo', 'LstmVanillaMeteo', 'LstmStackedMeteo', 'LstmBidireccionalMeteo']
+mls = [CnnMeteo(n_input_steps, n_features, n_output_steps, reg=True),
+       CnnMeteo(n_input_steps, n_features, n_output_steps, drop=True),
+       CnnMeteo(n_input_steps, n_features, n_output_steps, reg=True, drop=True),
+       LstmVanillaMeteo(n_input_steps, n_features, n_output_steps, reg=True),
+       LstmVanillaMeteo(n_input_steps, n_features, n_output_steps, drop=True),
+       LstmVanillaMeteo(n_input_steps, n_features, n_output_steps, reg=True, drop=True),
+       LstmStackedMeteo(n_input_steps, n_features, n_output_steps, reg=True),
+       LstmStackedMeteo(n_input_steps, n_features, n_output_steps, drop=True),
+       LstmStackedMeteo(n_input_steps, n_features, n_output_steps, reg=True, drop=True),
+       LstmBidireccionalMeteo(n_input_steps, n_features, n_output_steps, reg=True),
+       LstmBidireccionalMeteo(n_input_steps, n_features, n_output_steps, drop=True),
+       LstmBidireccionalMeteo(n_input_steps, n_features, n_output_steps, reg=True, drop=True)
+       ]
+mls_label = [
+    'CnnMeteo_r',
+    'CnnMeteo_d',
+    'CnnMeteo_r_d',
+    'LstmVanillaMeteo_r',
+    'LstmVanillaMeteo_d',
+    'LstmVanillaMeteo_r_d',
+    'LstmStackedMeteo_r',
+    'LstmStackedMeteo_d',
+    'LstmStackedMeteo_r_d',
+    'LstmBidireccionalMeteo_r',
+    'LstmBidireccionalMeteo_d',
+    'LstmBidireccionalMeteo_r_d'
+]
 
-for i in range(param,param+2):
+for i in range(param, param + 6):
     history = mls[i].model.fit(X, y, epochs=300, batch_size=32, validation_split=0.2, verbose=1)
     hist_df = pd.DataFrame(history.history)
-    with open("h_"+mls_label[i], mode='w') as f:
+    with open("h_" + mls_label[i], mode='w') as f:
         hist_df.to_json(f)
-    mls[i].model.save("m_"+mls_label[i])
-
+    mls[i].model.save("m_" + mls_label[i])
 
     # demonstrate prediction
-    x_input = X[9500+1,:,:]
-    x_input = x_input.reshape((1,x_input.shape[0],x_input.shape[1]))
+    x_input = X[9500 + 1, :, :]
+    x_input = x_input.reshape((1, x_input.shape[0], x_input.shape[1]))
     yhat = mls[i].model.predict(x_input, verbose=1)
 
     print("predicted")
-    y_real=y[9500+1,:]
-    y_real=y_real.reshape((1,y_real.shape[0]))
+    y_real = y[9500 + 1, :]
+    y_real = y_real.reshape((1, y_real.shape[0]))
     scalerC = pre_processor.scalers[2]
-    yhat =scalerC.inverse_transform(yhat)
+    yhat = scalerC.inverse_transform(yhat)
     scalerC = pre_processor.scalers[3]
     yreal = scalerC.inverse_transform(y_real)
-    testScore = math.sqrt(mean_squared_error(y_real[0,:], yhat[0,:]))
-    print('Test Score '+ mls_label[i] +': %.2f RMSE' % (testScore))
+    testScore = math.sqrt(mean_squared_error(y_real[0, :], yhat[0, :]))
+    print('Test Score ' + mls_label[i] + ': %.2f RMSE' % (testScore))
