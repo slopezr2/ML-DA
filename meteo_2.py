35729e7913cc47cc3c6b678beedb3599304f9ba2
slopezr2 <slopezr2@eafit.edu.co>
2020-08-24 15:59:45 +0200

    Experimentos Tunning redes V2

--- meteo_2.py
+++ meteo_2.py
@@ -41,44 +41,44 @@ mls_label = [ 'CnnMeteo_d',
 	  'CnvLstmMeteo',
 	  'CnvLstmMeteo_r_d'
 ]
-for semana in range(semanas):
-	n_input_steps = 24 * 7 * semanas[semana]
+for semana in semanas:
+	n_input_steps = 24 * 7 * semana
 	n_output_steps = 24 * 3
 	# pre-process data
 	pre_processor = Combiner()
 	X, y = pre_processor.combine(n_input_steps, n_output_steps, station82_t.Value.values, station82_w.Value.values,
-		                           station82_pm25.CONCENTRATION.values, station82_pm25.CONCENTRATION.values)
+                             station82_pm25.CONCENTRATION.values,station82_t.Date.dt.dayofweek.values,station82_t.Date.dt.hour.values , station82_pm25.CONCENTRATION.values)
 
 	# Create Model
 	n_train = 9500
 	n_features = X.shape[2]
 
+	for n_LSTM_hidden_layers in LSTM_hidden_layers:
+	
+		for n_cells in cells:
+		
+			mls = [ CnnMeteo(n_input_steps, n_features, n_output_steps, drop=True,n_LSTM_hidden_layers=n_LSTM_hidden_layers,n_cells=n_cells),
+						 CnvLstmMeteo(n_input_steps, n_features, n_output_steps, reg=False, drop=False,n_LSTM_hidden_layers=n_LSTM_hidden_layers,n_cells=n_cells),
+						 CnvLstmMeteo(n_input_steps, n_features, n_output_steps, reg=True, drop=True,n_LSTM_hidden_layers=n_LSTM_hidden_layers,n_cells=n_cells),
+						 ]
 
+			history = mls[param].fit(X, y, epochs=300, batch_size=32, validation_split=0.2, verbose=1)
+			hist_df = pd.DataFrame(history.history)
+			with open("h_input_"+str(semana)+"_hidden_"+str(n_LSTM_hidden_layers)+"_cells_"+str(n_cells)+"_"+ mls_label[param], mode='w') as f:
+					hist_df.to_json(f)
+			mls[param].model.save("m_" +str(semana)+"_hidden_"+str(n_LSTM_hidden_layers)+"_cells_"+str(n_cells)+"_"+ mls_label[param] + ".h5")
 
+			# demonstrate prediction
+			x_input = X[9500 + 1, :, :]
+			x_input = x_input.reshape((1, x_input.shape[0], x_input.shape[1]))
+			yhat = mls[param].predict(x_input, verbose=1)
 
-	mls = [ CnnMeteo(n_input_steps, n_features, n_output_steps, drop=True),
-		     CnvLstmMeteo(n_input_steps, n_features, n_output_steps, reg=False, drop=False),
-		     CnvLstmMeteo(n_input_steps, n_features, n_output_steps, reg=True, drop=True),
-		     ]
-
-
-	history = mls[param].fit(X, y, epochs=300, batch_size=32, validation_split=0.2, verbose=1)
-	hist_df = pd.DataFrame(history.history)
-	with open("h_input_"+str(semana)+"_hidden_"+str(n_LSTM_hidden_layers)+"_cells_"+str(n_cells)+ mls_label[param], mode='w') as f:
-		  hist_df.to_json(f)
-	mls[param].model.save("m_" + mls_label[param] + ".h5")
-
-	# demonstrate prediction
-	x_input = X[9500 + 1, :, :]
-	x_input = x_input.reshape((1, x_input.shape[0], x_input.shape[1]))
-	yhat = mls[param].predict(x_input, verbose=1)
-
-	print("predicted")
-	y_real = y[9500 + 1, :]
-	y_real = y_real.reshape((1, y_real.shape[0]))
-	scalerC = pre_processor.scalers[2]
-	yhat = scalerC.inverse_transform(yhat)
-	scalerC = pre_processor.scalers[3]
-	y_real = scalerC.inverse_transform(y_real)
-	testScore = mls[param].compare(y_real, yhat)
-	print('Test Score ' + mls_label[param] + ': %.2f RMSE' % (testScore))
+			print("predicted")
+			y_real = y[9500 + 1, :]
+			y_real = y_real.reshape((1, y_real.shape[0]))
+			scalerC = pre_processor.scalers[2]
+			yhat = scalerC.inverse_transform(yhat)
+			scalerC = pre_processor.scalers[3]
+			y_real = scalerC.inverse_transform(y_real)
+		testScore = mls[param].compare(y_real, yhat)
+		print('Test Score ' + mls_label[param] +str(semana)+"_hidden_"+str(n_LSTM_hidden_layers)+"_cells_"+str(n_cells)+"_"+ ': %.2f RMSE' % (testScore))
